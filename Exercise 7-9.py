8ec8d4d8dfe1b9b8ea5f8bc643602a024cd7a21a
awahbrian2-max <awahbrian2@gmail.com>
2025-12-23 22:13:40 +0100

    Refactor sandwich order processing and handle pastrami

--- Exercise 7-9.py
+++ Exercise 7-9.py
@@ -1,4 +1,5 @@
 sandwich_orders=["Tuna Sandwich","Meat Sandwich","Veggie Sandwich","Cheese Sandwich","pastrami","pastrami","pastrami"]
+print("Sorry We are out of Pastrami")
 finished_sandwiches=[]
 for sandwich_order in sandwich_orders:
     print("I made your ",sandwich_order)
@@ -7,4 +8,5 @@ for sandwich_order in sandwich_orders:
     finished_sandwiches.append(sandwich_order)
 print("\n")
 for finished_sandwich in finished_sandwiches:
+
     print(finished_sandwich,"Has been made")
